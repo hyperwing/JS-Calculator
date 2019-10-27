2a9316ce0aeab9e222fbbf7f1300c5be36ea7d30
sharonqiu1 <sharonqiu1@users.noreply.github.com>
2019-10-27 13:21:01 -0400

    Fixed pathing for tests.

--- test/memoryTest.js
+++ test/memoryTest.js
@@ -1,6 +1,6 @@
 // File created 10/25/2019 by Sri Ramya Dandu
 
 const rewire = require("rewire");
-const memoryFunctions = rewire("../memoryFunctions");
+const memoryFunctions = rewire("../scripts/memoryFunctions");
 expect = require('chai').expect;
 var memory = [1,2,3];
