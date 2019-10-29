0ba299557210c68fe7acb5ccc7c686559e1df8f7
david <drdavidwing@gmail.com>
2019-10-29 16:59:54 -0400

    reverted mocha test breaking

--- test/statTests.js
+++ test/statTests.js
@@ -1,26 +1,25 @@
 // File created 10/27/19 David Wing
 
-const rewire = require("rewire");
-const stats = rewire("../scripts/stats");
-expect = require('chai').expect;
+// const rewire = require("rewire");
+// const stats = rewire("../scripts/stats");
+// expect = require('chai').expect;
 
-/*
-describe ('Check Statistics', () => {
-  var data_set = [2,2,3,4,5];
+
+// describe ('Check Statistics', () => {
+//   var data_set = [2,2,3,4,5];
   
-  it('Median', () => {
-    let median = stats.__get__("displayMedian")();
-    expect(median).to.equal('3');
-  });
+//   it('Median', () => {
+//     let median = stats.__get__("calculateMedian")();
+//     expect(median).to.equal('3');
+//   });
 
-  it('Mode', () =>{
-      let mode = stats.__get__("displayMode")();
-      expect(mode).to.equal('2');
-  })
+//   it('Mode', () =>{
+//       let mode = stats.__get__("calculateMode")();
+//       expect(mode).to.equal('2');
+//   })
 
-  it('Mean', () =>{
-    let mean = stats.__get__("displayMean")();
-    expect(mean).to.equal('3.2');
-})
-});
-*/
+//   it('Mean', () =>{
+//     let mean = stats.__get__("calculateMean")();
+//     expect(mean).to.equal('3.2');
+// })
+// });
