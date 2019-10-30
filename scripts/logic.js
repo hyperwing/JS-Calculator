76dc1d3edace17844bad4ed3bb0886f9bb8b3c85
neelm88 <neelm88@gmail.com>
2019-10-30 00:14:51 -0400

    improved documentation

--- scripts/logic.js
+++ scripts/logic.js
@@ -8,11 +8,17 @@ var lastButtonOperator = false;
 var openParenthesisCount = 0;
 var lastNotIndex = -1;
 
+// Created 10/27/2019 by Neel Mansukhani
+// Edited 10/29/2019 by Neel Mansukhani
+// Handles number press events.
 function onNumberPress(symbol) {
     display = symbol;
     lastButtonOperator = false;
     updateDisplay();
 }
+
+// Created 10/29/2019 by Neel Mansukhani
+// Handles clear press events.
 function onClearPress(symbol) {
     if(symbol == "C") {
         calculations = [];
@@ -22,6 +28,17 @@ function onClearPress(symbol) {
     updateDisplay();
 }
 
+// Created 10/29/2019 by Neel Mansukhani
+// Handles delete press events.
+function onDeletePress() {
+    if(display == "1") {
+        display = "0";
+    }
+    updateDisplay();
+}
+
+// Created 10/29/2019 by Neel Mansukhani
+// Handles equal press events.
 function onEqualsPress() {
     if(isOperator(calculations[calculations.length - 1]) || calculations[calculations.length - 1] == '(') {
         calculations.push(display);
@@ -34,18 +51,24 @@ function onEqualsPress() {
     calculations = [];
     updateDisplay();
 }
+
+// Created 10/27/2019 by Neel Mansukhani
+// Edited 10/29/2019 by Neel Mansukhani
+// Updates display based on changes to display and calculations.
 function updateDisplay() {
     document.getElementById("display").innerHTML = display;
     document.getElementById("calculations").innerHTML = calculations.toString().replace(/,/g, " ");
 }
 
+// Created 10/29/2019 by Neel Mansukhani
+// Handles not operator press events.
 function onNotOperatorPress() {
     if(lastNotIndex == -1) {
         if(calculations[calculations.length - 1] == ')') {
             lastNotIndex = calculations.lastIndexOf('(');
             calculations.splice(lastNotIndex, 0, '!');
         } else {
-                if(isNaN(calculations[lastNotIndex+1])) {
+                if(isNaN(calculations[calculations.length - 1])) {
                     calculations.push(parseFloat(display));
                 }
                 calculations.splice(lastNotIndex, 0, '!');
@@ -58,6 +81,9 @@ function onNotOperatorPress() {
     lastButtonOperator = true;
     updateDisplay();
 }
+
+// Created 10/29/2019 by Neel Mansukhani
+// Handles logical operator press events not including the not operator.
 function onLogicalOperatorPress(operator) {
     if (isOperator(calculations[calculations.length - 1]) && lastButtonOperator) {
         calculations[calculations.length - 1] = operator;
@@ -71,6 +97,9 @@ function onLogicalOperatorPress(operator) {
     lastNotIndex = -1;
     updateDisplay();
 }
+
+// Created 10/29/2019 by Neel Mansukhani
+// Handles parenthesis press events.
 function onParenthesisPress(symbol) {
     if(symbol == '(' && calculations[calculations.length - 1] != ')' && isNaN(calculations[calculations.length - 1])) {
         calculations.push(symbol);
@@ -85,10 +114,16 @@ function onParenthesisPress(symbol) {
     lastNotIndex = -1;
     updateDisplay();
 }
+
+// Created 10/27/2019 by Neel Mansukhani
+// Edited 10/29/2019 by Neel Mansukhani: Removed not operator
+// Returns whether or not input is an operator.
 function isOperator(input) {
     return input == "&&" || input == "||" || input == "==";
 }
 
+// Created 10/29/2019 by Neel Mansukhani
+// Returns result of logical operations of value1 and value2 based on the given operator.
 function doLogic(value1, value2, operator) {
     switch (operator) {
         case "&&":
@@ -106,12 +141,13 @@ function doLogic(value1, value2, operator) {
     return value;
 }
 
+// Created 10/29/2019 by Neel Mansukhani
+// Returns calculated value of logical operations stored in arr.
 function calculate(arr) {
     let i = 0;
     let value1 = 0;
     let value2 = null;
     while(i < arr.length) {
-        console.log(arr)
         if(arr[i] == '(') {
             closeIndex = findCloseIndex(arr);
             value1 = calculate(arr.slice(i + 1,closeIndex));
@@ -154,7 +190,8 @@ function calculate(arr) {
     }
     return value1;
 }
-
+// Created 10/29/2019 by Neel Mansukhani
+// Given an array arr where the first index is an open parenthesis, it finds the index of the associated close parenthesis.
 function findCloseIndex(arr) {
     arr = arr.slice(1);
     var openParens = 1;
